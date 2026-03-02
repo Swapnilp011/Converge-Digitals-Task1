7486e66ef4cb50a8ce9cde95b8d1b777eb560c0a
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-03-02 14:43:19 +0530

    remove the react vita img and add the seach logo in the navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Navbar.css';
+import searchLogo from '../../assets/searchlogo.svg';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="container">
+            <div className="container" style={{ height: '100%' }}>
                 <ul className="navbar-nav">
                     <li><a href="#home" className="active">HOME</a></li>
                     <li><a href="#about">ABOUT US</a></li>
@@ -14,7 +15,9 @@ const Navbar = () => {
                     <li><a href="#news">NEWS</a></li>
                     <li><a href="#shop">SHOP</a></li>
                     <li><a href="#contact">CONTACT</a></li>
-                    <li style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center', color: 'white', cursor: 'pointer' }}>🔍</li>
+                    <li className="search-icon-container">
+                        <img src={searchLogo} alt="Search" className="search-logo" />
+                    </li>
                 </ul>
             </div>
         </nav>
